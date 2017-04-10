aca3560a959572c30264cf3ff85a5182cfb349d6
Alexander Hück <hueck.alexander@gmail.com>
2017-04-10 17:02:54 +0200

    google benchmark package detection

--- cmake/ExternalPackages.cmake
+++ cmake/ExternalPackages.cmake
@@ -1,29 +1,34 @@
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
 include(ExternalProject)
 
-# FIXME skip add step if already done to speed up compilation
+set(PREFIX_GOOGLE_BENCHMARK ${CMAKE_CURRENT_BINARY_DIR}/externalpackages/google_benchmark CACHE PATH "Path to Google Benchmark")
 
-ExternalProject_Add(project_google_benchmark
-  GIT_REPOSITORY https://github.com/google/benchmark.git
-  GIT_TAG v1.1.0
-  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/externalpackages/google_benchmark
-  CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/externalpackages/google_benchmark
-    -DCMAKE_BUILD_TYPE=Release
-)
-
-ExternalProject_Get_Property(project_google_benchmark INSTALL_DIR)
-set(project_google_benchmark_lib ${INSTALL_DIR}/lib)
-set(project_google_benchmark_lib_static ${INSTALL_DIR}/lib/libbenchmark.a)
-set(project_google_benchmark_include ${INSTALL_DIR}/include)
+find_package(benchmark)
 
 add_library(google_benchmark STATIC IMPORTED)
-add_dependencies(google_benchmark
-  project_google_benchmark
-)
+
+if(NOT ${BENCHMARK_FOUND})
+  message(STATUS "Adding Google Benchmark as external project to download")
+  ExternalProject_Add(project_google_benchmark
+    GIT_REPOSITORY https://github.com/google/benchmark.git
+    GIT_TAG v1.1.0
+    PREFIX ${PREFIX_GOOGLE_BENCHMARK}
+    CMAKE_ARGS
+      -DCMAKE_INSTALL_PREFIX=${PREFIX_GOOGLE_BENCHMARK}
+      -DCMAKE_BUILD_TYPE=Release
+  )
+  ExternalProject_Get_Property(project_google_benchmark INSTALL_DIR)
+  set(benchmark_LIBRARIES ${INSTALL_DIR}/lib/libbenchmark.a)
+  set(benchmark_INCLUDE_DIR ${INSTALL_DIR}/include)
+  add_dependencies(google_benchmark
+    project_google_benchmark
+  )
+endif()
+
 set_property(
   TARGET google_benchmark
-  PROPERTY IMPORTED_LOCATION ${project_google_benchmark_lib_static}
+  PROPERTY IMPORTED_LOCATION ${benchmark_LIBRARIES}
 )
 
 # List of all external include dirs (here only google benchmark)
-set(external_includes ${INSTALL_DIR}/include)
+set(external_includes ${benchmark_INCLUDE_DIR})
